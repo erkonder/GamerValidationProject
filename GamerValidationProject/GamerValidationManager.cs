3ed7758de7090c4f848fdd5f4a9c8226dc8e8ad7
Ahmet Erk Önder <aerkonder@outlook.com>
2021-02-14 17:06:22 +0300

    Add project files.

---
+++ GamerValidationProject/GamerValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamerValidationProject
+{
+    class GamerValidationManager : IGamerValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if(gamer.FirstName=="Erk" && gamer.LastName=="Önder" && gamer.BirthYear == 1995 && gamer.Id == 1 && gamer.NationalIdentity == 123456)
+            {
+                Console.WriteLine("Validation Tamamlandı");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Validation Tamamlanamadı");
+                return false;
+            }
+        }
+    }
+}
---
+++ GamerValidationProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GamerValidationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GamerManager gamer1 = new GamerManager(new GamerValidationManager());
+            gamer1.Add(new Gamer { FirstName ="Erk" , LastName="Önder" , BirthYear =1995 , Id = 1 , NationalIdentity = 123456 });
+            GamerManager gamer2 = new GamerManager(new GamerValidationManager());
+            gamer2.Add(new Gamer { FirstName = "Ege", LastName = "Onat", BirthYear = 1995, Id = 2, NationalIdentity = 123457 });
+
+        }
+    }
+}
